6b9fe578c43cec052628285ec326e2419ff63338
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-07-24 20:38:14 -0300

    Create Merendeira.java

---
+++ src/Merendeira.java
@@ -0,0 +1,21 @@
+package br.edu.ifs.academico;
+
+public class Merendeira extends Pessoa {
+
+	private String especialidade;
+	private String vulgo;
+	
+	public String getEspecialidade() {
+		return especialidade;
+	}
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
+	public String getVulgo() {
+		return vulgo;
+	}
+	public void setVulgo(String vulgo) {
+		this.vulgo = vulgo;
+	}
+	
+}

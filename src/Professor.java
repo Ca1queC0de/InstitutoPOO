3dc6d3314052faff4d3018b4cd9b6a8c1311af77
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-07-31 20:35:08 -0300

    Update Professor.java

--- src/Professor.java
+++ src/Professor.java
@@ -2,6 +2,10 @@ package br.edu.ifs.academico;
 
 public class Professor extends Pessoa {
 	
+	public Professor(String nome) {
+		super(nome);
+	}
+
 	private String formacao;
 
 	public String getFormacao() {

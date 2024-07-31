1b685ae4fafc96813b9454fe56001e5d1a95a527
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-07-31 20:33:39 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
+package br.edu.ifs.academico;
+
 public class App {
 
-    public static void main(String[] args) {
-        
-        Pessoa p = new Pessoa(null);
-
-    }
-
+	public static void main(String[] args) {
+		
+		Pessoa p = new Pessoa("Chris");
+		
+	}
+	
 }

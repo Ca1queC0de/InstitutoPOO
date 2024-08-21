0e1b5f2e1ea61c117d49c517d53061932fe2b105
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-08-21 20:16:00 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -12,12 +12,12 @@ public class App {
 		char sexo;
 		
 		Aluno[] listaAluno = new Aluno[10];
-		Merendeira[] merendeira = new Merendeira[10];
-		Pedagogo[] pedagogo = new Pedagogo[10];
+		Merendeira[] listaMerendeira = new Merendeira[10];
+		Pedagogo[] listaPedagogo = new Pedagogo[10];
 		Professor[] listaProfessor = new Professor[10];
-		Psicologo[] psicologo = new Psicologo[10];
-		Tecnico[] tecnico = new Tecnico[10];
-		Vigilante[] vigilante = new Vigilante[10];
+		Psicologo[] listaPsicologo = new Psicologo[10];
+		Tecnico[] listaTecnico = new Tecnico[10];
+		Vigilante[] listaVigilante = new Vigilante[10];
 		
 		int contAluno = 0;
 		int contMerendeira = 0;
@@ -75,30 +75,85 @@ public class App {
 				break;
 			case 5:
 				System.out.println("Cadastrando Técnico...\n");
+				System.out.print("Nome do Técnico: ");
+				nome = scanner.next();
+				Tecnico tecnico = new Tecnico(nome);
+				System.out.print("Local de Nascimento: ");
+				localNascimento = scanner.next();
+				tecnico.setLocalNascimento(localNascimento);
+				System.out.print("Sexo: ");
+				sexo = scanner.next().toUpperCase().charAt(0);
+				tecnico.setSexo(sexo);
+				listaTecnico[contTecnico] = tecnico;
+				contTecnico++;
 				break;
 			case 6:
 				System.out.println("Imprimindo Lista Técnicos...\n");
 				break;
 			case 7:
-				System.out.println("Cadastrando Pedagogo...\n");
+				System.out.println("Cadastrando Pedagogo...");
+				System.out.print("Nome do Pedagogo: ");
+				nome = scanner.next();
+				Pedagogo pedagogo = new Pedagogo(nome);
+				System.out.print("Local de Nascimento: ");
+				localNascimento = scanner.next();
+				pedagogo.setLocalNascimento(localNascimento);
+				System.out.print("Sexo: ");
+				sexo = scanner.next().toUpperCase().charAt(0);
+				pedagogo.setSexo(sexo);
+				listaPedagogo[contPedagogo] = pedagogo;
+				contPedagogo++;
 				break;
 			case 8:
 				System.out.println("Imprimindo Lista Pedagogos...\n");
 				break;
 			case 9:
-				System.out.println("Cadastrando Psicólogo...\n");
+				System.out.println("Cadastrando Psicólogo...");
+				System.out.print("Nome do Psicólogo: ");
+				nome = scanner.next();
+				Psicologo psicologo = new Psicologo(nome);
+				System.out.print("Local de Nascimento: ");
+				localNascimento = scanner.next();
+				psicologo.setLocalNascimento(localNascimento);
+				System.out.print("Sexo: ");
+				sexo = scanner.next().toUpperCase().charAt(0);
+				psicologo.setSexo(sexo);
+				listaPsicologo[contPsicologo] = psicologo;
+				contPsicologo++;
 				break;
 			case 10:
 				System.out.println("Imprimindo Lista Psicólogos...\n");
 				break;
 			case 11:
-				System.out.println("Cadastrando Merendeira...\n");
+				System.out.println("Cadastrando Merendeira...");
+				System.out.print("Nome da Merendeira: ");
+				nome = scanner.next();
+				Merendeira merendeira = new Merendeira(nome);
+				System.out.print("Local de Nascimento: ");
+				localNascimento = scanner.next();
+				merendeira.setLocalNascimento(localNascimento);
+				System.out.print("Sexo: ");
+				sexo = scanner.next().toUpperCase().charAt(0);
+				merendeira.setSexo(sexo);
+				listaMerendeira[contMerendeira] = merendeira;
+				contMerendeira++;
 				break;
 			case 12:
 				System.out.println("Imprimindo Lista Merendeiras...\n");
 				break;
 			case 13:
-				System.out.println("Cadastrando Vigilante...\n");
+				System.out.println("Cadastrando Vigilante...");
+				System.out.print("Nome do Vigilante: ");
+				nome = scanner.next();
+				Vigilante vigilante = new Vigilante(nome);
+				System.out.print("Local de Nascimento: ");
+				localNascimento = scanner.next();
+				vigilante.setLocalNascimento(localNascimento);
+				System.out.print("Sexo: ");
+				sexo = scanner.next().toUpperCase().charAt(0);
+				vigilante.setSexo(sexo);
+				listaVigilante[contVigilante] = vigilante;
+				contVigilante++;
 				break;
 			case 14:
 				System.out.println("Imprimindo Lista Vigilantes...\n");

f535ecfc0c6be66c7637d6b5846d0e78561e5dab
Caíque <149615073+CaiqueCodeSD@users.noreply.github.com>
2024-09-10 20:04:59 -0300

    Create Templario.java

---
+++ src2/Templario.java
@@ -0,0 +1,29 @@
+package sistemaJogoGuerreiro;
+
+//Essa classe herda atributos de Guerreiro por isso usa o "extends"
+public class Templario extends Guerreiro{
+
+	//Nesse construtor encontramos o "super" que pega os atributos obrigatórios da classe "Geurreiro"
+	public Templario(String nome, int vidas) {
+		super(nome, vidas);
+	}
+
+	// Declaração das variáveis
+	private int nivelArmadura;
+	private int velocidadeCavalo;
+	
+	// Métodos para definir e pegar valores
+	public int getNivelArmadura() {
+		return nivelArmadura;
+	}
+	public void setNivelArmadura(int nivelArmadura) {
+		this.nivelArmadura = nivelArmadura;
+	}
+	public int getVelocidadeCavalo() {
+		return velocidadeCavalo;
+	}
+	public void setVelocidadeCavalo(int velocidadeCavalo) {
+		this.velocidadeCavalo = velocidadeCavalo;
+	}
+	
+}

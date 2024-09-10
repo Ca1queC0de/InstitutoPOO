e7ccbb4e8b21e502c034adc0b20edbb3aa6c5862
Caíque <149615073+CaiqueCodeSD@users.noreply.github.com>
2024-09-10 20:04:06 -0300

    Create Arqueiro.java

---
+++ src2/Arqueiro.java
@@ -0,0 +1,29 @@
+package sistemaJogoGuerreiro;
+
+//Essa classe herda atributos de Guerreiro por isso usa o "extends"
+public class Arqueiro extends Guerreiro {
+
+	//Nesse construtor encontramos o "super" que pega os atributos obrigatórios da classe "Geurreiro"
+	public Arqueiro(String nome, int vidas) {
+		super(nome, vidas);
+	}
+	
+	// Declaração das variáveis
+	private int habilidadeArco;
+	private int alcance;
+	
+	// Métodos para definir e pegar valores
+	public int getHabilidadeArco() {
+		return habilidadeArco;
+	}
+	public void setHabilidadeArco(int habilidadeArco) {
+		this.habilidadeArco = habilidadeArco;
+	}
+	public int getAlcance() {
+		return alcance;
+	}
+	public void setAlcance(int alcance) {
+		this.alcance = alcance;
+	}
+	
+}

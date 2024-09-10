79e8267fb3c8a74bc153d4f0ee9c3d695639c126
Caíque <149615073+CaiqueCodeSD@users.noreply.github.com>
2024-09-10 20:05:36 -0300

    Create Menu.java

---
+++ src2/Menu.java
@@ -0,0 +1,16 @@
+package sistemaJogoGuerreiro;
+
+public class Menu {
+
+	/* O método abaixo é do tipo void pois não retorna nada
+	Em algumas partes do código foi utilizado o "\n" para pular linha */
+	public void mostrarMenu() {
+		System.out.println("\n[1] Criar Espadachim\n"
+				+ "[2] Criar Arqueiro\n"
+				+ "[3] Criar Mago\n"
+				+ "[4] Criar Valquíria\n"
+				+ "[5] Criar Templário\n"
+				+ "[0] Sair\n");
+	}
+	
+}
